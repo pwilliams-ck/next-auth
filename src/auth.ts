d4a93c5d533a9d73645d7983d8114e50a49adebf
pwilliams-ck <pwilliams@cloudkey.io>
2024-04-21 21:27:25 -0500

    add jwt and session token callbacks for user id.

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,20 @@ import { db } from '@/lib/db';
 
 // This is for getting Prisma to work on the edge.
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      return token;
+    },
+  },
+
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,

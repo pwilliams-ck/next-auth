bdbf16178fc27f55cc1f5bbb5f41bfbf62140cfa
pwilliams-ck <pwilliams@cloudkey.io>
2024-04-22 16:40:19 -0500

    add file upload route & component.

---
+++ src/app/(platform)/upload/_components/upload-form.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+const UploadForm = ({ url }: { url: string }) => {
+  return (
+    <form
+      className=""
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        const file = (e.target as HTMLFormElement).file.files?.[0]!;
+
+        const image = await fetch(url, {
+          body: file,
+          method: 'PUT',
+          headers: {
+            'Content-Type': file.type,
+            'Content-Disposition': `attachment; filename="${file.name}"`,
+          },
+        });
+
+        window.location.href = image.url.split('?')[0];
+      }}
+    >
+      <input name="file" type="file" accept="image/png, image/jpeg" />
+      <button type="submit">Upload</button>
+    </form>
+  );
+};
+
+export default UploadForm;
